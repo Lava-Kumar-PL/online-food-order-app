d1d98f05b53c9276969146ff7b9c770259a90a9d
Lava-Kumar-PL <kumar1956.11.1@gmail.com>
2024-11-18 19:11:03 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Oroder your Favourite food here</h2>
+        <p> Choose from diversre menu with finest ingredients.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import'./Home.css'
+import Header from '../../components/Header/Header'
+
+import MenuExplore from '../../components/MenuExplore/MenuExplore'
+import { useState } from "react";
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload';
+
+const Home = () => {
+
+  const [category,setCategory] = useState("All");
+
+  return (
+    <div>
+      <Header/>
+      <MenuExplore category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload/>
+    </div>
+  )
+}
+
+export default Home
